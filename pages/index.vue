3965fd4c01d32bec5699ef4834717216435d60cc
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2023-12-08 15:02:39 -0300

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>This site is a test involving nuxt3 and netlify deploy process</h1>
+    <img src="/cute-cat.jpg" alt="cute cat">
+    <img src="~/assets/cute-parrot.jpeg" alt="cute parrot">
+  </div>
+</template>
+
+<style scoped lang="sass">
+img
+  height: 150px
+  width: auto
+  margin-right: 10px
+</style>
